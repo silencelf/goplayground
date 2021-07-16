b7dc14593ab4b67322cbf1e7ddf478ef962f8fe2
kkou <a@b.c>
2021-07-16 19:14:54 +0800

    wtf

--- stack/stack.go
+++ stack/stack.go
@@ -1,48 +1,48 @@
 package stack
 
 import (
-    "sync"
-    "errors"
+	"errors"
+	"sync"
 )
 
 type Stack struct {
-    lock    sync.Mutex
-    value   []int
+	lock  sync.Mutex
+	value []int
 }
 
 func NewStack() *Stack {
-    return &Stack { sync.Mutex{}, make([]int, 0)}
+	return &Stack{sync.Mutex{}, make([]int, 0)}
 }
 
 func (s *Stack) Push(v int) {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    s.value = append(s.value, v)
+	s.value = append(s.value, v)
 }
 
 func (s *Stack) Pop() (int, error) {
-    s.lock.Lock()
-    defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-    l := len(s.value)
-    if l == 0 {
-        return 0, errors.New("Empty Stack")
-    }
-    res := s.value[l - 1]
-    s.value = s.value[:l-1]
+	l := len(s.value)
+	if l == 0 {
+		return 0, errors.New("Empty Stack")
+	}
+	res := s.value[l-1]
+	s.value = s.value[:l-1]
 
-    return res, nil
+	return res, nil
 }
 
 func (s *Stack) Peek() (int, error) {
-    if len(s.value) == 0 {
-        return 0, errors.New("Empty Stack")
-    }
+	if len(s.value) == 0 {
+		return 0, errors.New("Empty Stack")
+	}
 
-    return s.value[0], nil
+	return s.value[len(s.value)-1], nil
 }
 
 func (s *Stack) IsEmpty() bool {
-    return len(s.value) == 0
+	return len(s.value) == 0
 }
