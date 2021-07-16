0d4715f133731201cea4b3a17f98a761967e72f9
kkou <a@b.c>
2021-07-16 17:29:04 +0800

    half way solution committed

--- stack/main/main.go
+++ stack/main/main.go
@@ -5,14 +5,28 @@ import (
     "playground/stack"
 )
 
-func main() {
+func dailyTemperatures(temperatures []int) []int {
     s := stack.NewStack()
-    s.Push(1)
-    s.Push(2)
-    s.Push(3)
-
     for !s.IsEmpty() {
         fmt.Println(s.Pop())
     }
-    fmt.Println(s.Pop())
+
+    for _, i := range(temperatures) {
+        top, err := s.Peek()
+        if err != nil || top >= i {
+            s.Push(i)
+        }
+    }
+    fmt.Println("stack: ", s)
+
+    return temperatures
+}
+
+func main() {
+    temperatures := []int{73,74,75,71,69,72,76,73}
+    fmt.Println(temperatures)
+    expected := []int{1,1,4,2,1,1,0,0}
+    fmt.Println(expected)
+    result := dailyTemperatures(temperatures)
+    fmt.Println(result)
 }
