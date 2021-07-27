3e075948186b39cd7a6ad1eaba76d243b4bca351
kkou <a@b.c>
2021-07-27 17:25:38 +0800

    add a log analyzer

---
+++ logs/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/oschwald/geoip2-golang"
+)
+
+func main() {
+	file, err := os.Open("ip.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	c := make(map[string]int)
+	db, err := geoip2.Open("country.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	for scanner.Scan() {
+		ip := scanner.Text()
+		name := printCountry(db, ip)
+		if _, ok := c[name]; ok {
+			c[name]++
+		} else {
+			c[name] = 1
+		}
+	}
+
+	fmt.Println(c)
+}
+
+func printCountry(db *geoip2.Reader, ipAddr string) string {
+	// If you are using strings that may be invalid, check that ip is not nil
+	ip := net.ParseIP(ipAddr)
+	record, err := db.Country(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("Chinese country name: %v\n", record.Country.Names["zh-CN"])
+	//fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
+	//fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+	return record.Country.Names["zh-CN"]
+}
+
