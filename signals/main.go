b62bd0e79f5a10b37c1c53312e099ab490933534
= <=>
2021-08-01 12:21:06 +0800

    remove bad signal

--- signals/main.go
+++ signals/main.go
@@ -13,17 +13,13 @@ func main() {
 	c := make(chan os.Signal)
 	//监听指定信号 ctrl+c kill
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM,
-		syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+		syscall.SIGQUIT)
 	go func() {
 		for s := range c {
 			switch s {
 			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
 				fmt.Println("Program Exit...", s)
 				gracefullExit()
-			case syscall.SIGUSR1:
-				fmt.Println("usr1 signal", s)
-			case syscall.SIGUSR2:
-				fmt.Println("usr2 signal", s)
 			default:
 				fmt.Println("other signal", s)
 			}
