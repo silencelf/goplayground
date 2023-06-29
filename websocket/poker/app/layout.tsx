d4502a562798695bd9273332eeb22338fe10a466
kent kou <kent.kou@activenetwork.com>
2023-06-29 16:57:40 +0800

    try the styles

--- websocket/poker/app/layout.tsx
+++ websocket/poker/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Poker face',
+  title: 'Poker mon!',
   description: 'The simple poker for you.',
 }
 
